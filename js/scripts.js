43e50c2efa0f7dd7a4eff9c7502fe3cf651a9616
AlexZimms <136646234+AlexZimms@users.noreply.github.com>
2024-03-06 14:15:20 -0800

    Adding pokemonList
    First implementation of lists/arrays into js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList [
+    {   
+        name: 'Charizard',
+        height: 5.7,
+        types: ['fire', 'flying']
+    },
+
+    {
+        name: 'Ampharos',
+        height: 4.7,
+        types: 'electric',
+    }
+
+    {
+        name: 'Aggron',
+        height: 6.11,
+        types: ['steel', 'rock']
+    }
+]
