8a9ebcdd9f32a10c184c062db271a81474fba9a3
AlexZimms <136646234+AlexZimms@users.noreply.github.com>
2024-03-06 18:25:00 -0800

    Adjusting for loop
    Adding conditionals and comments,
    cleaning up code for loops.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
 let pokemonList = [
-    {   
-        name: 'Charizard',
+    {   name: 'Charizard',
         height: 5.7,
         types: ['fire', 'flying']
     },
 
-    {
-        name: 'Ampharos',
+    {   name: 'Ampharos',
         height: 4.7,
         types: 'electric',
     },
 
-    {
-        name: 'Aggron',
+    {   name: 'Aggron',
         height: 6.11,
         types: ['steel', 'rock']
     }
 ];
 
-for (let i=0; i < pokemonList.length; i++){
-    document.write("pokemonList.name")
+    // Creating a list of pokemon and their heights. Adding conditionals to highlight large pokemon!
+    
+for (let i=0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ")  ");
+
+    if (pokemonList[i].height > 6){
+        document.write(" Wow that's huge!");
+    }
 
 
-}
+
+};
