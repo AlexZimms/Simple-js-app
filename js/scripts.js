1dd58f1d80d5c6585470d8efd9786cc41407b8e9
AlexZimms <136646234+AlexZimms@users.noreply.github.com>
2024-03-08 12:17:23 -0800

    Added css link and js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,3 @@
-let pokemonList = [
-    {   name: 'Charizard',
-        height: 5.7,
-        types: ['fire', 'flying']
-    },
-
-    {   name: 'Ampharos',
-        height: 4.7,
-        types: 'electric',
-    },
-
-    {   name: 'Aggron',
-        height: 6.11,
-        types: ['steel', 'rock']
-    }
-];
-
-    // Creating a list of pokemon and their heights. Adding conditionals to highlight large pokemon!
-    
-for (let i=0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ")  ");
-
-    if (pokemonList[i].height > 6){
-        document.write(" Wow that's huge!");
-    }
-
-
-
-};
+alert('Hello world');
+let favoriteFood = 'Pizza';
+document.write (favoriteFood);
