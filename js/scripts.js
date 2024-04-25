043786d5cac01280d24ab5f27f3585a450369903
AlexZimms <136646234+AlexZimms@users.noreply.github.com>
2024-04-24 17:49:58 -0700

    Implimented Bootstrap code
    I was able to follow the instructions, but this made the modal non fucntional.

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,19 @@ let pokemonRepository = (function () {
     function addListItem(pokemon){
         let pokemonList = document.querySelector(".pokemon-list");
         let listPokemon = document.createElement("li");
+        listPokemon.classList.add("list-group-item", "list-group-item-action");
+      
+
         let button = document.createElement("button");
         button.innerText = pokemon.name;
-        button.classList.add("button-class");
+        button.classList.add('btn', 'btn-secondary', 'btn-block');
+        button.setAttribute('type', 'button');
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("data-target", "#pokemonModal")
+
         listPokemon.appendChild(button);
         pokemonList.appendChild(listPokemon);
+        
         button.addEventListener('click', function() {
           showDetails(pokemon);
         }); 
@@ -52,55 +60,57 @@ let pokemonRepository = (function () {
         console.error(e);
       });
     }
-    function showModal(item) {
-            let modalContainer = document.querySelector('#modal-container');
-            modalContainer.innerHTML = '';
-            let modal = document.createElement('div');
-            modal.classList.add('modal');
+    // function showModal(item) {
+    //         let modalContainer = document.querySelector('#modal-container');
+    //         modalContainer.innerHTML = '';
+    //         let modal = document.createElement('div');
+    //         modal.classList.add('modal');
 
-            let closeButtonElement = document.createElement('button');
-            closeButtonElement.classList.add('modal-close');
-            closeButtonElement.innerText = 'Close';
-            closeButtonElement.addEventListener('click', hideModal);
+    //         let closeButtonElement = document.createElement('button');
+    //         closeButtonElement.classList.add('modal-close');
+    //         closeButtonElement.innerText = 'Close';
+    //         closeButtonElement.addEventListener('click', hideModal);
 
-            let titleElement = document.createElement('h1');
-            titleElement.innerText = item.name;
+    //         let titleElement = document.createElement('h1');
+    //         titleElement.innerText = item.name;
 
-            let heightElement = document.createElement('p');
-            heightElement.innerText = 'Height: ' + item.height;
+    //         let heightElement = document.createElement('p');
+    //         heightElement.innerText = 'Height: ' + item.height;
 
-            let imageElement = document.createElement('img');
-            imageElement.classList.add('modal-image');
-            imageElement.src = item.imageUrl;
+    //         let imageElement = document.createElement('img');
+    //         imageElement.classList.add('modal-image');
+    //         imageElement.src = item.imageUrl;
 
-            modal.appendChild(closeButtonElement);
-            modal.appendChild(titleElement);
-            modal.appendChild(heightElement);
-            modal.appendChild(imageElement);
-            modalContainer.appendChild(modal);
+    //         modal.appendChild(closeButtonElement);
+    //         modal.appendChild(titleElement);
+    //         modal.appendChild(heightElement);
+    //         modal.appendChild(imageElement);
+    //         modalContainer.appendChild(modal);
 
-            modalContainer.classList.add('is-visible');
-          function hideModal() {
-            modalContainer.classList.remove('is-visible');
-            modalContainer.addEventListener('click', (e) => {
-            let target = e.target;
-            if (target === modalContainer) {
-                hideModal();
-            }
-            // adding listener for additional modal closing
-            window.addEventListener('keydown', (e) => {
-              if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-                hideModal();
-              }
-            });
-          });
+    //         modalContainer.classList.add('is-visible');
+            
+
+    //       function hideModal() {
+    //         modalContainer.classList.remove('is-visible');
+    //         modalContainer.addEventListener('click', (e) => {
+    //         let target = e.target;
+    //         if (target === modalContainer) {
+    //             hideModal();
+    //         }
+    //         // adding listener for additional modal closing
+    //         window.addEventListener('keydown', (e) => {
+    //           if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    //             hideModal();
+    //           }
+    //         });
+    //       });
         
-        document.querySelector('.button-class').addEventListener(
-            'click', function () {
-                showModal(pokemon);
-            })
-        };
-        };
+    //     document.querySelector('.button-class').addEventListener(
+    //         'click', function () {
+    //             showModal(pokemon);
+    //         })
+    //     };
+    //     };
         // linking css to hide modal
         
         
